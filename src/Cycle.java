5e739108b8ebbd0f927aac460182b7ffe4a461ed
Kushagra Sharma <kushagra8888@gmail.com>
2015-10-20 15:38:52 +0530

    add some boilerplate code

---
+++ src/Cycle.java
@@ -0,0 +1,9 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by kushasharma on 10/20/2015.
+ */
+public class Cycle {
+    private Set<Edge> cycleEdges = new HashSet();
+}
---
+++ src/Vertex.java
@@ -0,0 +1,14 @@
+/**
+ * Created by kushasharma on 10/20/2015.
+ */
+public class Vertex {
+    private String label;
+
+    public void Vertex(String label){
+        this.label = label;
+    }
+
+    public String getLabel(){
+        return this.label;
+    }
+}
